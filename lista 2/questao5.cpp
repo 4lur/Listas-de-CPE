486bc06f41af5b5e70573b1bf88036258efd1917
4lur <156508035+4lur@users.noreply.github.com>
2024-04-03 18:08:31 -0300

    Create questao5.cpp

---
+++ lista 2/questao5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float lar,comp,area;
+
+    cout<<"Digite a largura da sala: ";
+    cin>>lar;
+
+    cout<<"Digite o comprimento da sala: ";
+    cin>>comp;
+
+    area = lar * comp;
+
+    cout<<"A area da sala e: "<<area<<" M";
+
+    return 0;
+}
