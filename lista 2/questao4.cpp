f9e59cf7df9b810e8876e44055a27462b0cbc780
4lur <156508035+4lur@users.noreply.github.com>
2024-04-03 19:10:30 -0300

    Update questao4.cpp

--- lista 2/questao4.cpp
+++ lista 2/questao4.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main() 
+{
+    //unico meio que encontrei para fazer sem for.
+    cout<<("********\t\t    ***    \t\t   *   \t         *          \n");
+    cout<<("*      *\t\t  *     *  \t\t  ***  \t       *   *        \n");
+    cout<<("*      *\t\t*         *\t\t ***** \t     *       *      \n");
+    cout<<("*      *\t\t*         *\t\t   *   \t   *           *    \n");
+    cout<<("*      *\t\t*         *\t\t   *   \t *               *  \n");
+    cout<<("*      *\t\t*         *\t\t   *   \t   *           *    \n");
+    cout<<("*      *\t\t*         *\t\t   *   \t     *       *      \n");
+    cout<<("*      *\t\t  *     *  \t\t   *   \t       *   *        \n");
+    cout<<("********\t\t    ***    \t\t   *   \t         *          \n");
+
+    return 0;
+}
