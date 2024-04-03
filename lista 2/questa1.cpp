9d6ed2f2729d5d73caee0506b002d95812c29762
4lur <156508035+4lur@users.noreply.github.com>
2024-04-03 17:54:51 -0300

    Create questa1.cpp

---
+++ lista 2/questa1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 7+3*6/2-1, y = 2%2+2*2/2, z = (3*9*(3+(9*3/(3))));
+
+    cout<<"O valor de X: "<<x<<"\nO valor Y: "<<y<<"\nO valor de Z: "<<z;
+
+    return 0;
+}
