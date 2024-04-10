32e6e288595608b83f6580af2b8e2ce64bc5528a
4lur <156508035+4lur@users.noreply.github.com>
2024-04-10 09:15:26 -0300

    Create questao01.cpp

---
+++ lista 3/questao01.cpp
@@ -0,0 +1,29 @@
+/*
+Lista 3, questão 01
+Aluno: Vinicius Magno da Silva Nascimento
+Matricula: 222005528
+Programa: Calculadora de media/consumo por Km/L
+*/
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    float qntL, qntKm,kml;
+
+    cout<<"Digite a quantidade de litros abastecidos: ";
+    cin>>qntL;
+
+    cout<<"Digite a quantidade de kilometros percorrido: ";
+    cin>>qntKm;
+
+    //calcula a media do consumo
+    kml = qntKm/qntL;
+
+    //exibe a media de consumo
+    cout<<"A media de consumo do seu carro e: "<<fixed<<setprecision(1)<<kml<<" Km/L"<<endl;
+
+    return 0;
+}
