aec34555f240c793a9777d9410d4ecc3d497cbdc
4lur <156508035+4lur@users.noreply.github.com>
2024-04-10 09:21:32 -0300

    Create questao02.cpp

---
+++ lista 3/questao02.cpp
@@ -0,0 +1,37 @@
+/*
+Lista 3, questão 2
+Aluno: Vinicius Magno da Silva Nascimento
+Matricula: 222005528
+Programa: Calcula e recomenda se deve abastecer com álcool ou gasolina
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float prec_gosolina, prec_alcool, proporcao;
+
+    //Registra o valor da gasolina
+    cout<<"Digite o valor da gasolina: ";
+    cin>>prec_gosolina;
+
+    //Registra o valor do alcool
+    cout<<"Digite o valor do Alcool: ";
+    cin>>prec_alcool;
+
+    //calcula a proporção
+    proporcao = prec_alcool/prec_gosolina;
+
+    //Faz a recomandação do alcool X gasolina
+    if(proporcao <= 0.7)
+    {
+        cout<<"E recomendado abastecer com Alcool.";
+        
+    }
+
+    else
+        cout<<"E recomendado abastecer gosolina.";
+
+    return 0;
+}
