3422febb9ded94f16e888f2666c66fde6979922a
4lur <156508035+4lur@users.noreply.github.com>
2024-04-12 08:58:10 -0300

    Update and rename questao1letraA.cpp to questao01letraA.cpp

---
+++ Lista 4/questao01letraA.cpp
@@ -0,0 +1,26 @@
+/*
+Lista 4
+Questão: 1, Letra A
+Aluno: Vinicius Magno da Silva Nascimento
+Matricula: 222005528
+Programa: Calcula fatorial
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 10; // Número de termos
+    double e = 1.0; // Valor inicial de e
+    int fatorial = 1; // Valor inicial do fatorial
+
+    // Loop para somar os termos
+    for (int i = 1; i <= n; ++i) {
+        fatorial *= i; // Calcula o fatorial
+        e += 1.0 / fatorial; // Adiciona o termo ao valor de e
+    }
+
+    cout << "O valor aproximado de e com " << n << " termos e: " << e << endl;
+
+    return 0;
+}
